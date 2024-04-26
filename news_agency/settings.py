b3886c0c7d58124283f16801e990d55f9c2c8bdc
vital <vitaliyburkalo81@gmail.com>
2024-04-26 14:42:42 +0300

    fixed settings of the project to include the app pulse

--- news_agency/settings.py
+++ news_agency/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'news_agency'
+    'pulse'
 ]
 
 MIDDLEWARE = [
@@ -124,4 +124,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = "news_agency.Redactor"
+AUTH_USER_MODEL = "pulse.Redactor"
