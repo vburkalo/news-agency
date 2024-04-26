9098a080c81513e828bf2426371d560c9a566e3b
vital <vitaliyburkalo81@gmail.com>
2024-04-26 15:10:47 +0300

    updated urls for the pulse app

---
+++ pulse/urls.py
@@ -0,0 +1,57 @@
+from django.urls import path
+
+from pulse.views import (
+    index,
+    TopicListView,
+    TopicCreateView,
+    TopicUpdateView,
+    TopicDeleteView,
+    RedactorListView,
+    RedactorDetailView,
+    RedactorCreateView,
+    RedactorUpdateView,
+    RedactorDeleteView,
+    NewspaperListView,
+    NewspaperDetailView,
+    NewspaperCreateView,
+    NewspaperUpdateView,
+    NewspaperDeleteView,
+)
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("topics/", TopicListView.as_view(), name="topics"),
+    path("topics/create/", TopicCreateView.as_view(), name="topic-create"),
+    path("topics/<int:pk>/update/", TopicUpdateView.as_view(), name="topic-update"),
+    path("topics/<int:pk>/delete/", TopicDeleteView.as_view(), name="topic-delete"),
+    path("redactors/", RedactorListView.as_view(), name="redactors"),
+    path("redactors/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail"),
+    path("redactors/create/", RedactorCreateView.as_view(), name="redactor-create"),
+    path(
+        "redactors/<int:pk>/update/",
+        RedactorUpdateView.as_view(),
+        name="redactor-update",
+    ),
+    path(
+        "redactors/<int:pk>/delete/",
+        RedactorDeleteView.as_view(),
+        name="redactor-delete",
+    ),
+    path("newspapers/", NewspaperListView.as_view(), name="newspapers"),
+    path(
+        "newspapers/<int:pk>/", NewspaperDetailView.as_view(), name="newspaper-detail"
+    ),
+    path("newspapers/create/", NewspaperCreateView.as_view(), name="newspaper-create"),
+    path(
+        "newspapers/<int:pk>/update/",
+        NewspaperUpdateView.as_view(),
+        name="newspaper-update",
+    ),
+    path(
+        "newspapers/<int:pk>/delete/",
+        NewspaperDeleteView.as_view(),
+        name="newspaper-delete",
+    ),
+]
+
+app_name = "pulse"
