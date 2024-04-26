da94cf043a05600953824f2f0926c864a189db55
vital <vitaliyburkalo81@gmail.com>
2024-04-26 15:22:42 +0300

    updated forms py inside pulse app for pulse views

---
+++ pulse/forms.py
@@ -0,0 +1,55 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+from django.contrib.auth import get_user_model
+
+from pulse.models import Topic, Redactor, Newspaper
+
+
+class TopicForm(ModelForm):
+    class Meta:
+        model = Topic
+        fields = ["name"]
+
+
+class RedactorForm(UserCreationForm):
+    years_of_experience = forms.IntegerField(required=True)
+
+    class Meta(UserCreationForm.Meta):
+        model = Redactor
+        fields = UserCreationForm.Meta.fields + (
+            "years_of_experience",
+            "first_name",
+            "last_name",
+            "email",
+        )
+
+
+class NewspaperForm(ModelForm):
+    topic = forms.ModelMultipleChoiceField(
+        queryset=Topic.objects.all(),
+        widget=forms.CheckboxSelectMultiple(),
+        required=False,
+    )
+    publishers = forms.ModelMultipleChoiceField(
+        queryset=Redactor.objects.all(),
+        widget=forms.CheckboxSelectMultiple(),
+        required=False,
+    )
+
+    class Meta:
+        model = Newspaper
+        fields = "__all__"
+        widgets = {
+            "published_date": forms.DateInput(attrs={"type": "date"}),
+            "content": forms.Textarea(attrs={"rows": 4, "cols": 40}),
+        }
+
+
+class NewspaperSearchForm(forms.Form):
+    title = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by title"}),
+    )
