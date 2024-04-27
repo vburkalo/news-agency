1fa30dfe1c3b8fe74be80c72ad21e541e9ffe969
vital <vitaliyburkalo81@gmail.com>
2024-04-27 17:57:06 +0300

    refactored urls py file to improve readability of the code

--- pulse/urls.py
+++ pulse/urls.py
@@ -20,13 +20,41 @@ from pulse.views import (
 
 urlpatterns = [
     path("", index, name="index"),
-    path("topics/", TopicListView.as_view(), name="topics"),
-    path("topics/create/", TopicCreateView.as_view(), name="topic-create"),
-    path("topics/<int:pk>/update/", TopicUpdateView.as_view(), name="topic-update"),
-    path("topics/<int:pk>/delete/", TopicDeleteView.as_view(), name="topic-delete"),
-    path("redactors/", RedactorListView.as_view(), name="redactors"),
-    path("redactors/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail"),
-    path("redactors/create/", RedactorCreateView.as_view(), name="redactor-create"),
+    path(
+        "topics/",
+        TopicListView.as_view(),
+        name="topics"
+    ),
+    path(
+        "topics/create/",
+        TopicCreateView.as_view(),
+        name="topic-create"
+    ),
+    path(
+        "topics/<int:pk>/update/",
+        TopicUpdateView.as_view(),
+        name="topic-update"
+    ),
+    path(
+        "topics/<int:pk>/delete/",
+        TopicDeleteView.as_view(),
+        name="topic-delete"
+    ),
+    path(
+        "redactors/",
+        RedactorListView.as_view(),
+        name="redactors"
+    ),
+    path(
+        "redactors/<int:pk>/",
+        RedactorDetailView.as_view(),
+        name="redactor-detail"
+    ),
+    path(
+        "redactors/create/",
+        RedactorCreateView.as_view(),
+        name="redactor-create"
+    ),
     path(
         "redactors/<int:pk>/update/",
         RedactorUpdateView.as_view(),
@@ -37,11 +65,21 @@ urlpatterns = [
         RedactorDeleteView.as_view(),
         name="redactor-delete",
     ),
-    path("newspapers/", NewspaperListView.as_view(), name="newspapers"),
     path(
-        "newspapers/<int:pk>/", NewspaperDetailView.as_view(), name="newspaper-detail"
+        "newspapers/",
+        NewspaperListView.as_view(),
+        name="newspapers"
     ),
-    path("newspapers/create/", NewspaperCreateView.as_view(), name="newspaper-create"),
+    path(
+        "newspapers/<int:pk>/",
+        NewspaperDetailView.as_view(),
+        name="newspaper-detail"
+    ),
+    path(
+        "newspapers/create/",
+        NewspaperCreateView.as_view(),
+        name="newspaper-create"
+    ),
     path(
         "newspapers/<int:pk>/update/",
         NewspaperUpdateView.as_view(),
