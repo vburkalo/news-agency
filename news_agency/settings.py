eb16ba506920a07b5745c2cc5617df96d79dbd24
vital <vitaliyburkalo81@gmail.com>
2024-04-27 11:56:37 +0300

    added new templates options and also added new assets root for the project in news agency settings

--- news_agency/settings.py
+++ news_agency/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "pulse.context_processors.cfg_assets_root",
             ],
         },
     },
@@ -123,6 +124,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+ASSETS_ROOT = "/static/assets"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
