f5464fcb5e1a6b32763ecc6a0f0a162bd571efcd
vital <vitaliyburkalo81@gmail.com>
2024-04-29 15:22:01 +0300

    add news-agency-7vww.onrender.com to ALLOWED_HOSTS

--- news_agency/settings.py
+++ news_agency/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "news-agency-7vww.onrender.com"]
 
 # SECURITY SETTINGS:
 
