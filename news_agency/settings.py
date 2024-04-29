9b2d117f9fac63d473597d158671cd05e31d15e3
vital <vitaliyburkalo81@gmail.com>
2024-04-29 14:16:55 +0300

    staticfiles managing

--- news_agency/settings.py
+++ news_agency/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from dotenv import load_dotenv
 from pathlib import Path
 import os
+import dj_database_url
 
 load_dotenv()
 
@@ -58,6 +59,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -98,6 +100,11 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
+
+DATABASE_URL = "postgres://qjqsczzx:O8_fmbmZrDqnh7qGRyc4hTb02dbrnV1x@lucky.db.elephantsql.com/qjqsczzx"
 
 
 # Password validation
@@ -141,6 +148,8 @@ STATICFILES_DIRS = [
 ]
 
 ASSETS_ROOT = "/static/assets"
+
+STATIC_ROOT = "staticfiles/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
